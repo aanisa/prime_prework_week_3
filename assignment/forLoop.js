09d0c0e12157a0a6da984442f931106463dc15ae
Anisa <Anisa@Anisas-MacBook-Pro.local>
2017-02-11 17:05:46 -0600

    Assingment Complete

--- assignment/forLoop.js
+++ assignment/forLoop.js
@@ -4,10 +4,5 @@ function shoesOnTheBus( kidsOnTheBus ){
   }
   return i*2;
 }
-  // this function accepts the number of kids on the bus
-  // assume each kid on the bus is wearing 2 shoes
-  // write a for loop that counts how many shoes are on the bus
-  // this function should return the number of shoes on the bus
-// end shoesOnTheBus
 
 console.log(shoesOnTheBus(8));
--- assignment/loopsAndConditions.js
+++ assignment/loopsAndConditions.js
@@ -9,12 +9,6 @@ var newArray = [];
     }
   }
   return newArray;
-};
+}
 
 console.log(onlyEvens(array));
-
-
-
-// complete this function so that given an array full of
-// numbers, it returns a new array containing only the even ones
-// for example `onlyEvens([1,2,3,4]) === [2,4]`
